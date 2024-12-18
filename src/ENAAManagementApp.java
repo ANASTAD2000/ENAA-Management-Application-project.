3c664823af8d841fc4d4efeede8a1bd70aae2a25
ANASTAD2000 <anasbaclibr@gmai.com>
2024-12-18 10:43:51 +0100

    display the first User interface

---
+++ src/ENAAManagementApp.java
@@ -0,0 +1,15 @@
+public class ENAAManagementApp {
+    public static void main(String[] args) {
+        System.out.println("*****************************************");
+        System.out.println("*          ENAA Management App          *");
+        System.out.println("*****************************************");
+        System.out.println("* 1. Manage Students                    *");
+        System.out.println("* 2. Manage Trainers                    *");
+        System.out.println("* 3. Manage Classes                     *");
+        System.out.println("* 4. Search by ID or Name               *");
+        System.out.println("* 5. Show All Classes and Their Content *");
+        System.out.println("* 6. Exit                               *");
+        System.out.println("*****************************************");
+        System.out.print("Enter your choice: ");
+    }
+}
